7f3508f8dc4acccbc4bb440f3eb4662c5cb68a9e
Landen Bailey <54867037+landiinii@users.noreply.github.com>
2024-05-21 04:18:24 -0600

    final updates

--- import_script.sql
+++ import_script.sql
@@ -3,7 +3,9 @@ CREATE TABLE IF NOT EXISTS public.populations
     id serial NOT NULL,
     zip_code int NOT NULL,
     population_count int,
-    PRIMARY KEY (id)
+	location_id int,
+    PRIMARY KEY (id),
+	FOREIGN KEY (location_id) REFERENCES public.location (location_id)
 );
 TRUNCATE populations;
 
@@ -13,7 +15,9 @@ CREATE TABLE IF NOT EXISTS public.incomes
     zip_code int NOT NULL,
     median_income int,
     margin_error int,
-    PRIMARY KEY (id)
+    location_id int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (location_id) REFERENCES public.location (location_id)
 );
 TRUNCATE incomes;
 
@@ -23,7 +27,17 @@ from 'C:\Users\Public\Downloads\d211-main\populationPerZip.csv'
 DELIMITER ','
 CSV HEADER;
 
+UPDATE populations
+SET location_id=subquery.location_id
+FROM (SELECT location_id, zip FROM location) AS subquery
+WHERE populations.zip_code=subquery.zip;
+
 Copy incomes(zip_code, median_income, margin_error) 
 from 'C:\Users\Public\Downloads\d211-main\MI_ZIP.csv'
 DELIMITER ','
 CSV HEADER;
+
+UPDATE incomes
+SET location_id=subquery.location_id
+FROM (SELECT location_id, zip FROM location) AS subquery
+WHERE incomes.zip_code=subquery.zip;
